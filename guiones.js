45de90d88dee0a0ac400aec488ca47eb1c8cf82d
paola <pebitaa5795@hotmail.com>
2023-07-09 23:59:01 -0600

    Tarea

--- guiones.js
+++ guiones.js
@@ -1,71 +1,31 @@
-consola.log(cajaParrafos.childNodes)
-consola.log(cajaParrafos.niños[4].innerText = 'cambio desde JS')
+const div = document.getElementById('miDiv');
+console.log(div.textContent); // Output: "Hola, mundo!"
+
+div.textContent = "¡Hola, OpenAI!";
+console.log(div.textContent); // Output: "¡Hola, OpenAI!"
+
+const div = document.getElementById('miDiv');
+console.log(div.innerHTML); // Output: "Hola, <span>mundo</span>!"
+
+div.innerHTML = "¡Hola, <strong>OpenAI</strong>!";
+console.log(div.innerHTML); // Output: "¡Hola, <strong>OpenAI</strong>!"
 
 
-console.log(enlace.getAttribute('href'))
-formulario.setAttribute('action', '#')
-console.log(formulario.getAttribute('action'))
+const div = document.getElementById('miDiv');
+console.log(div.outerHTML);
+// Output: "<div id="miDiv">Hola, <span>mundo</span>!</div>"
+
+div.outerHTML = '<p id="nuevoParrafo">¡Hola a todos!</p>';
+console.log(div.outerHTML);
+// Output: "<p id="nuevoParrafo">¡Hola a todos!</p>"
 
 
-let metaTags = document.querySelectorAll('meta')
+// Crear un nuevo elemento <li>
+const nuevoElementoLi = document.createElement('li');
+nuevoElementoLi.textContent = 'Nuevo elemento';
 
-for (const tag of metaTags) {
-console.log(tag.getAttribute('content'))
- }
+// Obtener el elemento padre (ul) al que queremos agregar el nuevo elemento
+const lista = document.getElementById('miLista');
 
-formulario.action = "/controladores/guardar.php"
-
-consola.log(enlace.href)
-consola.log(boton2.id)
-console.log(boton2.getAttribute('id'))
-
-let input = documento.querySelector('input')
-
-
-consola.log(input.value)
-console.log(titulo.getAttribute('class').replace('fw-bold',''))
-consola.log(titulo.classList)
-
-titulo.classList.add('fs-6')
-titulo.classList.remove('text-success')
-titulo.classList.add('text-danger')
-
-
-
-console.log(texto[1].textContent);
-texto[1].textContent = "hola, altere el texto"
-
-consola.log(cajaParrafos.textContent)
-consola.log(cajaParrafos.innerText)
-consola.log(cajaParrafos.innerHTML)
-consola.log(cajaParrafos.outerHTML)
-
-cajaParrafos.innerHTML = "<tabla></tabla>"
-
-const tablaGenerada = documento.createElement('tabla')
-const parrafoNuevo = documento.createElement('p')
-
-parrafoNuevo.textContent = "parrafo creado desde JS"
-parrafoNuevo.classList.add('text-info', 'fw-bold')
-
-cajaParrafos.appendChild(tablaGenerada)
-cajaParrafos.appendChild(parrafoNuevo)
-
-consola.log(cajaParrafos.firstChild)
-consola.log(cajaParrafos.firstElementChild)
-consola.log(cajaParrafos.lastChild)
-consola.log(cajaParrafos.lastElementChild)
-consola.log(cajaParrafos.nextElementSibling)
-consola.log(cajaParrafos.siguienteHermano)
-consola.log(cajaParrafos.previousElementSibling.previousElementSibling)
-consola.log(cajaParrafos.parentElement)
-
-
-let enlaceHermano = documento.getElementById('email').parentElement.parentElement.parentElement.nextElementSibling
-enlaceHermano.href = "https://cit.mindef.mil.gt"
-enlaceHermano.innerText = "Ir a AUTOCOM"
-consola.log(enlaceHermano)
-
-documento.addEventListener('dblclick', (e)=>{
-    consola.log(e)
-})
+// Agregar el nuevo elemento como un nodo al final de la lista de elementos hijos
+lista.appendChild(nuevoElementoLi);
--- indice.php
+++ indice.php
@@ -1,20 +1,122 @@
-<title>Manipulación del DOM</title>
-</cabeza>
-<cuerpo>
- <h1 id="tituloPagina" class="text-success">MANIPULACIÓN DEL DOM</h1>
- <h1 id="tituloPagina" class="text-success fw-bold">MANIPULACIÓN DEL DOM</h1>
-    <Br>
- <button id="botonClick">Click</button>
-    <Br>
-@@ -22,9 +22,9 @@
-    <Br>
- <div class="row">
- <div class="col">
- <form action="">
-            <forma >
- <label for="email">Correo electrónico</label>
- <input type="email" name="email" id="email">
- <input type="email" name="email" id="email" value="texto de prueba ">
-            </forma>
-        </Div>
-    </Div>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Información Personal</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Font Awesome icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      padding: 20px;
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    form {
+      max-width: 500px;
+      margin: 0 auto;
+      background-color: #f2f2f2;
+      padding: 30px;
+      border-radius: 10px;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 10px;
+      font-weight: bold;
+    }
+
+    input[type="text"],
+    input[type="email"],
+    select {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 15px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+    }
+
+    select {
+      appearance: none;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      background-repeat: no-repeat;
+      background-position: right center;
+      background-size: 10px;
+    }
+
+    button {
+      padding: 10px 20px;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    .social-icons {
+      text-align: center;
+      margin-top: 20px;
+    }
+
+    .social-icons a {
+      display: inline-block;
+      margin-right: 10px;
+      color: #333;
+      font-size: 20px;
+    }
+
+  </style>
+</head>
+
+<body>
+  <h1 class="titulo">formulario</h1>
+
+  <form class="formulario">
+    <label for="nombres">Nombres:</label>
+    <input type="text" class="nombres" placeholder="Ingrese sus nombres" required>
+
+    <label for="apellidos">Apellidos:</label>
+    <input type="text" class="apellidos" placeholder="Ingrese sus apellidos" required>
+
+    <label for="telefono">Número DPI:</label>
+    <input type="text" class="telefono" placeholder="Ingrese su número de teléfono" required>
+
+
+    <div id="miDiv"> <span></span></div>
+
+    <label for="sexo">Sexo:</label>
+    <select class="sexo" required>
+      <option value="">Selecciona sexo</option>
+      <option value="Femenino">Femenino</option>
+      <option value="Masculino">Masculino</option>
+      <option value="Otro">Otro</option>
+    </select>
+
+    <button class="botonEnviar">Enviar</button>
+  </form>
+
+  <div class="box">
+  <p class="primary">¡Bienvenidos a mi sitio web!</p>
+  <p class="p2">Este es un ejemplo de texto personalizado.</p>
+  <p class="p3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, asperiores.</p>
+  <p class="success">Gracias por visitar.</p>
+</div>
+
+
+ 
+
+  <!-- Font Awesome script -->
+  <script src="guiones.js"></script>
+</body>
+
+</html>
